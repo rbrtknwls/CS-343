26424883f21027147448ad37561ba8f322b5573c
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-12 00:11:50 -0400

    init

---
+++ A1/Source/q3floatconstant.h
@@ -0,0 +1,18 @@
+_Coroutine FloatConstant {
+	char ch;											// character passed by cocaller
+	// YOU ADD MEMBERS HERE
+	void main();										// coroutine main
+  public:
+	enum { EOT = '\003' };								// end of text
+	_Event Match {										// last character match
+	  public:
+		double value;									// floating-point value
+		Match( double value ) : value( value ) {}
+	};
+	_Event Error {};									// last character invalid
+
+	void next( char c ) {
+		ch = c;											// communication input
+		resume();										// activate
+	}
+};
