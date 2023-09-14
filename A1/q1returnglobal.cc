d10d6897b904433d6ad43b0e27d4b4b8e6aa14fd
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-13 21:22:16 -0400

    q1

--- A1/q1returnglobal.cc
+++ A1/q1returnglobal.cc
@@ -4,28 +4,37 @@
 using namespace std;
 #include <unistd.h>										// access: getpid
 
-struct Ex1 { short int code; };
-struct Ex2 { int code; };
-struct Ex3 { long int code; };
+bool flag1 = false;
+bool flag2 = false;
+bool flag3 = false;
 
 intmax_t eperiod = 10000;								// exception period
 int randcnt = 0;
+
 int Rand() { randcnt += 1; return rand(); }
 
 double rtn1( double i ) {
-  if ( Rand() % eperiod == 0 ) { throw Ex1{ (short int)Rand() }; } // replace
+    if ( Rand() % eperiod == 0 ) { flag1 = true; }
 	return i + Rand();
 }
 double rtn2( double i ) {
-  if ( Rand() % eperiod == 0 ) { throw Ex2{ Rand() }; } // replace
+    if ( Rand() % eperiod == 0 ) { flag2 = true; }
 	return rtn1( i ) + Rand();
 }
 double rtn3( double i ) {
-  if ( Rand() % eperiod == 0 ) { throw Ex3{ Rand() }; }	// replace
+    if ( Rand() % eperiod == 0 ) { flag3 = true; }
 	return rtn2( i ) + Rand();
 }
 
-static intmax_t convert( const char * str );			// copy from https://student.cs.uwaterloo.ca/~cs343/examples/convert.h
+static intmax_t convert( const char * str ) {			// convert C string to integer
+    char * endptr;
+    errno = 0;											// reset
+    intmax_t val = strtoll( str, &endptr, 10 );			// attempt conversion
+    if ( errno == ERANGE ) throw std::out_of_range("");
+    if ( endptr == str ||								// conversion failed, no characters generated
+         *endptr != '\0' ) throw std::invalid_argument(""); // not at end of str ?
+    return val;
+}
 
 int main( int argc, char * argv[] ) {
 	intmax_t times = 100000000, seed = getpid();		// default values
@@ -52,11 +61,22 @@ int main( int argc, char * argv[] ) {
 	int rc = 0, ec1 = 0, ec2 = 0, ec3 = 0;
 
 	for ( int i = 0; i < times; i += 1 ) {
-		try { rv += rtn3( i ); rc += 1; }				// replace
-		// analyse exception
-		catch( Ex1 ev ) { ev1 += ev.code; ec1 += 1; }	// replace
-		catch( Ex2 ev ) { ev2 += ev.code; ec2 += 1; }	// replace
-		catch( Ex3 ev ) { ev3 += ev.code; ec3 += 1; }	// replace
+        flag1 = false;                                  // Reset Flag1
+        flag2 = false;                                  // Reset Flag2
+        flag3 = false;                                  // Reset Flag3
+
+		rv += rtn3( i ); rc += 1;
+
+        if ( flag1 ) {
+            ev1 += (short int) Rand(); ec1 += 1;                   // Increase ev1 if flag1 raised
+        }
+        if ( flag2 ) {
+            ev2 += (int) Rand(); ec2 += 1;                         // Increase ev2 if flag2 raised
+        }
+        if ( flag3 ) {
+            ev3 += (long int) Rand(); ec3 += 1;                    // Increase ev3 if flag3 raised
+        }
+
 	} // for
 	cout << "randcnt " << randcnt << endl;
 	cout << "normal result " << rv << " exception results " << ev1 << ' ' << ev2 << ' ' << ev3 << endl;
