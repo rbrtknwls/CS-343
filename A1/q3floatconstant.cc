ddf4d1d92183fe3f4a83fc4abff7e47c902aa42d
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-18 15:36:12 -0400

    more

--- A1/q3floatconstant.cc
+++ A1/q3floatconstant.cc
@@ -1,9 +1,16 @@
-#include <iostream>
-#include "/u/r2knowles/CS-343/A1/q3floatconstant.h"
-
 using namespace std;
+_Coroutine FloatConstant {
+    char ch;											// character passed by cocaller
+    void main();
 
-void FloatConstant::main() {
-    suspend();
-    cout << "Silly Billy" << endl;
-}
+    public:
+    enum { EOT = '\003' };								// end of text
+    _Event Match {										// last character match
+        public:
+        double value;									// floating-point value
+        Match( double value ) : value( value ) {}
+    };
+
+    _Event Error {};									// last character invalid
+    void next( char c );
+};
