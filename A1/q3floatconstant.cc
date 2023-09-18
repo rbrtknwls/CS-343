31ef110ec3af33732741a865b8835b41b9118aa2
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-17 22:58:40 -0400

    oops

--- A1/q3floatconstant.cc
+++ A1/q3floatconstant.cc
@@ -3,24 +3,7 @@
 
 using namespace std;
 
-_Coroutine FloatConstant {
-	char ch;											// character passed by cocaller
-	void main() {
-        suspend();
-        cout << "Silly Billy" << endl;
-
-    }
-  public:
-	enum { EOT = '\003' };								// end of text
-	_Event Match {										// last character match
-	  public:
-		double value;									// floating-point value
-		Match( double value ) : value( value ) {}
-	};
-	_Event Error {};									// last character invalid
-
-	void next( char c ) {
-		ch = c;											// communication input
-		resume();										// activate
-	}
-};
+void FloatConstant::main() {
+    suspend();
+    cout << "Silly Billy" << endl;
+}
