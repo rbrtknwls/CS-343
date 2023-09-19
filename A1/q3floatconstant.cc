e6ea48eb9be43f10f1d815f11eea10537c68e27d
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-19 14:46:22 -0400

    more

--- A1/q3floatconstant.cc
+++ A1/q3floatconstant.cc
@@ -155,7 +155,7 @@ int main( int argc, char * argv[] ) {
             } catch ( FloatConstant::Match & match ) {
                 std::cout << "match" << std::endl;
 
-            } catch ( ... ) {
+            } catch ( FloatConstant::Error & error ) {
                 std::cout << "ERROR" << std::endl;
             }
 
