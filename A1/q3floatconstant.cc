52d92724fcc48e7a3a462e218b152e89b8b19a73
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-19 00:04:44 -0400

    more

--- A1/q3floatconstant.cc
+++ A1/q3floatconstant.cc
@@ -109,9 +109,9 @@ void FloatConstant::next(char c) {
     ch = c; // Read in the character;
     try {
         resume();
-    } (catch Match mc) {
+    } catch ( Match mc ) {
         cout << "MATCH" << endl;
-    } (catch Error er) {
+    } catch ( Error er ) {
         cout << "ERROR" << endl;
     }
 }
