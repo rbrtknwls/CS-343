ae32c0715d0b6b7875b9e57ec52da7131c188aa0
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-29 22:43:22 -0400

    more

--- A2/q1binsertsort.cpp
+++ A2/q1binsertsort.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 
-
 #include "q1binsertsort.h"
 
 int main( int argc, char * argv[] ) {
-    std::cout << "hi" << std::endl;
+
+    istream * infile = &cin;							// default value
+
+    struct cmd_error {};
+
+    try {
+        switch ( argc ) {
+            case 2:
+                try {								    // open input file first as output creates file
+                    infile = new ifstream( argv[1] );
+                } catch( uFile::Failure & ) {		    // open failed ?
+                    cerr << "Error! Could not open input file \"" << argv[3] << "\"" << endl;
+                    throw cmd_error();
+                } // try
+                // FALL THROUGH
+            case 1:										// defaults
+                break;
+            default:								    // wrong number of options
+                throw cmd_error();
+        } // switch
+    } catch( ... ) {									// catch any
+        cerr << "Usage: " << argv[0]
+             << " [ input-file ] " << endl;
+        exit( EXIT_FAILURE );							// TERMINATE
+    } // try
+
+    for (;;) {
+        int numberOfValuesToSort;
+        *infile >> numberOfValuesToSort;
+
+        for (int i = 0; i < numberOfValuesToSort; i++) {
+            int currValue;
+
+            *infile >> currValue
+        }
+    }
 }
 
