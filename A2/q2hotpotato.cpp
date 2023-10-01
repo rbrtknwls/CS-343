356ba97526af64482908b406459b4620f7f45652
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-30 20:38:29 -0400

    more

--- A2/q2hotpotato.cpp
+++ A2/q2hotpotato.cpp
@@ -14,7 +14,7 @@ int main( int argc, char * argv[] ) {
     PRNG rng(seed);
 
     int games = 5;
-    int players = 2;
+    int players = rng(2, 10);
 
     try {
         if ( argc >= 4 ) {
@@ -23,17 +23,20 @@ int main( int argc, char * argv[] ) {
         } // if
 
         if ( argc >= 2 ) {
-            if ( *argv[1] != 'd' ) { games = stoi(argv[1]); }
+            if ( *argv[1] != 'd' ) { games = stoi( argv[1] ); }
         } // if
 
         if ( argc > 3 ){
-            if ( *argv[2] != 'd' ) { players = stoi(argv[2]); }
+            if ( *argv[2] != 'd' ) { players = stoi( argv[2] ); }
         } // if
 
         if ( argc == 4 ) {
             if ( *argv[3] != 'd' ) {
-                seed = stoi(argv[3]);
-            }
+                seed = stoi( argv[3] );
+                rng.set_seed( seed );
+
+                if ( *argv[2] == 'd' ) { players = rng( 2, 10 ) }
+            } // if
         } // if
 
 
@@ -42,7 +45,7 @@ int main( int argc, char * argv[] ) {
              << "  [ games | ’d’ [ players | ’d’ [ seed | ’d’ ] ] ] " << endl;
         exit(EXIT_FAILURE);                            // TERMINATE
     } // try
-
+    cout << players << " players in the game" << endl;
 
     cout << "hi" << endl;
 
