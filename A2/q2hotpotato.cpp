007ade77b14b65915bdc28810892cf1ba92394eb
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-30 20:20:26 -0400

    more

--- A2/q2hotpotato.cpp
+++ A2/q2hotpotato.cpp
@@ -17,22 +17,22 @@ int main( int argc, char * argv[] ) {
     int players = 2;
 
     try {
-        if (argc >= 4) {
+        if ( argc >= 4 ) {
             cerr << "Too many arguments!" << endl;
             throw cmd_error();
         } // if
 
-        if (argc > 2) {
-            if ( argv[1] != 'd' ) { games = argv[1] }
+        if ( argc > 2 ) {
+            if ( argv[1] != 'd' ) { games = argv[1]; }
         } // if
 
-        if (argc > 3){
-            if ( argv[2] != 'd' ) { players = argv[2] }
+        if ( argc > 3 ){
+            if ( argv[2] != 'd' ) { players = argv[2]; }
         } // if
 
-        if (argc > 4) {
+        if ( argc > 4 ) {
             if ( argv[3] != 'd' ) {
-                seed = argv[3]
+                seed = argv[3];
             }
         } // if
 
