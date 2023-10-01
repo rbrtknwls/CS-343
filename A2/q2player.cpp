df2bcc12e3b23f3d84bb69423972ed617106db6c
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-01 15:17:17 -0400

    more

--- A2/q2player.cpp
+++ A2/q2player.cpp
@@ -2,10 +2,10 @@
 #include <uPRNG.h>
 
 #include "q2player.h"
-
+#include "q2potato.h"
 
 Player::Player( PRNG & prng, unsigned int id, Potato & potato ) : prng ( prng ), id ( id ), potato ( potato ) {
 
     std::cout << "MADE";
-    
+
 }
