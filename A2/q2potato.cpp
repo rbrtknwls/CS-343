bd0db9755bf9c105593c4a0d3e4746b503fedb1b
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-30 22:23:37 -0400

    more

--- A2/q2potato.cpp
+++ A2/q2potato.cpp
@@ -1,6 +1,6 @@
 #include <uPRNG.h>
 #include "q2potato.h"
-/*
+
 Potato::Potato( PRNG &prng, unsigned int maxTicks ) : prng( prng ) {
     currentTicks = prng(1, maxTicks);
 }
@@ -8,4 +8,3 @@ Potato::Potato( PRNG &prng, unsigned int maxTicks ) : prng( prng ) {
 void Potato::reset(unsigned int maxTicks) {
     currentTicks = prng(1, maxTicks);
 }
-*/
