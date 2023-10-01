7ce29fc084e70c3ebdb00bbca5e772a9b441bbe8
rbrtknwls <r2knowle@uwaterloo.ca>
2023-09-30 22:22:12 -0400

    more

--- A2/q2player.cpp
+++ A2/q2player.cpp
@@ -3,8 +3,10 @@
 
 #include "q2player.h"
 
+/*
 Player::Player( PRNG & prng, unsigned int id, Potato & potato ) : prng ( prng ), id ( id ), potato ( potato ) {
 
     std::cout << "MADE";
 }
 
+*/
--- A2/q2potato.cpp
+++ A2/q2potato.cpp
@@ -1,6 +1,6 @@
 #include <uPRNG.h>
 #include "q2potato.h"
-
+/*
 Potato::Potato( PRNG &prng, unsigned int maxTicks ) : prng( prng ) {
     currentTicks = prng(1, maxTicks);
 }
@@ -8,3 +8,4 @@ Potato::Potato( PRNG &prng, unsigned int maxTicks ) : prng( prng ) {
 void Potato::reset(unsigned int maxTicks) {
     currentTicks = prng(1, maxTicks);
 }
+*/
