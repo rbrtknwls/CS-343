a6b48e9d798ff9390f7ddc60eb6776730041dc89
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-01 18:28:09 -0400

    more

--- A2/q3.cpp
+++ A2/q3.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 using namespace std;
-static volatile long int shared = 0; // volatile to prevent dead-code removal
+
+static volatile long int shared = 0;					// volatile to prevent dead-code removal
 static intmax_t iterations = 500000000;
+
 _Task increment {
         void main() {
             for ( decltype(iterations) i = 0; i < iterations; i += 1 ) {
-                shared += 1; // multiple increments to increase pipeline size
+                shared += 1;								// multiple increments to increase pipeline size
                 shared += 1;
             } // for
         } // increment::main
 }; // increment
-int main( int argc, char *
-argv[ ] ) {
+
+int main( int argc, char * argv[] ) {
     intmax_t processors = 1;
     struct cmd_error {};
-    try { // process command-line arguments
+    try {												// process command-line arguments
         switch ( argc ) {
             case 3: processors = convert( argv[2] ); if ( processors <= 0 ) throw cmd_error{};
             case 2: iterations = convert( argv[1] ); if ( iterations <= 0 ) throw cmd_error{};
-            case 1: break; // use defaults
+            case 1: break;								// use defaults
             default: throw cmd_error{};
         } // switch
-    } catch( . . . ) {
+    } catch( ... ) {
         cout << "Usage: " << argv[0] << " [ iterations (> 0) [ processors (> 0) ] ]" << endl;
-        exit( EXIT_FAILURE ); // TERMINATE!
+        exit( EXIT_FAILURE );							// TERMINATE!
     } // try
-    uProcessor p[processors - 1]; // create additional kernel threads
+
+    uProcessor p[processors - 1];						// create additional kernel threads
     {
         increment t[processors == 1 ? 2 : processors];
     } // wait for tasks to finish
