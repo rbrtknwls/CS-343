115414a2fad3b6f250e8defe0f86978e0d44c1da
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-03 19:50:34 -0400

    even more

--- tester.cpp
+++ tester.cpp
@@ -2,6 +2,6 @@
 
 
 int main ( int argc, char * argv[] ) {
-    std::string file1 = str(argv[1].c_str());
-    system("./" + file);
+    std::string file1 = std::str(argv[1]);
+    system("./" + file1);
 }
