a0923945125691cad95ef79b0d904e16c4130820
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-03 19:36:06 -0400

    more

--- A2/q2hotpotato.cpp
+++ A2/q2hotpotato.cpp
@@ -96,6 +96,8 @@ int main( int argc, char * argv[] ) {
 
         delete Player::umpire;                                                       // delete last player
         players.clear();                                                             // clear the player list
+
+        if ( gamesToPlay != 1 ) { cout << endl << endl }                             // separate games by spaces
     }
 
     return 0;
