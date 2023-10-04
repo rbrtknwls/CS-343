8c67823cad45231ecbfc0e5a5fec252d2c498ad9
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-03 20:02:09 -0400

    more

--- tester.cpp
+++ tester.cpp
@@ -6,7 +6,7 @@ int main ( int argc, char * argv[] ) {
     for ( int i = 0; i < 2; i++ ) {
         for (int x = 0; x < 2; x++ ) {
             for (int z = 0; z < 2; z++ ) {
-                std::string arguments = std::to_string(i) + " " + std::to_string(x) + " " + std::to_string(z) + " > temp.a";
+                std::string arguments = " " + std::to_string(i) + " " + std::to_string(x) + " " + std::to_string(z) + " > temp.a";
                 std::string file1 = std::string("./") + argv[1] + arguments;
 
                 system(file1.c_str());
