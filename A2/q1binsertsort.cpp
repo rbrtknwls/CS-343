7f7a9f921d19b0eeb2992b222b1e724810b1b46e
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-03 23:16:45 -0400

    more

--- A2/q1binsertsort.cpp
+++ A2/q1binsertsort.cpp
@@ -148,7 +148,7 @@ int main( int argc, char * argv[] ) {
                     }
                 }
 
-            } catch (Binsertsort<TYPE>::Sentinel &sentinel) {
+            } catch ( ... ) {
                 cout << endl;
                 cout << "UERE";
                 break;
