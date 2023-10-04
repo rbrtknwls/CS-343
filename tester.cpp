0eb5fa18f63ee482db300b80d60a2b91ec094014
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-03 22:47:39 -0400

    more

--- tester.cpp
+++ tester.cpp
@@ -43,6 +43,9 @@ int main ( int argc, char * argv[] ) {
         std::string file1 = std::string("./") + argv[1] + arg1;
         std::string file2 = std::string("./") + argv[2] + arg2;
 
+        system(file1.c_str());
+        system(file2.c_str());
+        
         system(("diff temp"+ std::to_string(testNum) +".a temp.b").c_str());
     }
 
