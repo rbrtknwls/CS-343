b33898c3167ae0b0c88fbf98389031cfa35be74d
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-03 22:37:44 -0400

    more

--- tester.cpp
+++ tester.cpp
@@ -31,8 +31,19 @@ int main ( int argc, char * argv[] ) {
         }
 
         currentFile.close();
+    }
 
+    for ( int testNum = 0; testNum < 10; testNum++ ) {
 
+        fileToRead = "input" + std::to_string(testNum) + ".txt";
+
+        std::string arg1 = fileToRead + " > temp.a 2> temp.a";
+        std::string arg2 = fileToRead + " > temp.b 2> temp.b";
+
+        std::string file1 = std::string("./") + argv[1] + arg1;
+        std::string file2 = std::string("./") + argv[2] + arg2;
+
+        system("diff temp.a temp.b");
     }
 
 
