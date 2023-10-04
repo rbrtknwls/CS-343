22ca8d35741e7c3cc4ee670a434e1349856c40a3
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-03 20:06:59 -0400

    poop

--- tester.cpp
+++ tester.cpp
@@ -16,7 +16,7 @@ int main ( int argc, char * argv[] ) {
                 if ( z == 0 ) { arguments += "d "; }
                 else { arguments += std::to_string(z) + " "; }
 
-                std::string arguments +=  " > temp.a 2> temp.a";
+                arguments += " > temp.a 2> temp.a";
                 std::string file1 = std::string("./") + argv[1] + arguments;
 
                 system(file1.c_str());
