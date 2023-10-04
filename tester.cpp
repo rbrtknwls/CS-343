94443e4140037eee23f650dfd56de2b17932db81
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-03 20:06:36 -0400

    more

--- tester.cpp
+++ tester.cpp
@@ -6,15 +6,15 @@ int main ( int argc, char * argv[] ) {
     for ( int i = 0; i < 5; i++ ) {
         for (int x = 0; x < 5; x++ ) {
             for (int z = 0; z < 5; z++ ) {
-                std::string arguments = " "
-                if ( i == 0 ) { arguments += "d "}
-                else { arguments += std::to_string(i) + " " }
+                std::string arguments = " ";
+                if ( i == 0 ) { arguments += "d "; }
+                else { arguments += std::to_string(i) + " "; }
 
-                if ( x == 0 ) { arguments += "d "}
-                else { arguments += std::to_string(x) + " " }
+                if ( x == 0 ) { arguments += "d "; }
+                else { arguments += std::to_string(x) + " "; }
 
-                if ( z == 0 ) { arguments += "d "}
-                else { arguments += std::to_string(z) + " "}
+                if ( z == 0 ) { arguments += "d "; }
+                else { arguments += std::to_string(z) + " "; }
 
                 std::string arguments +=  " > temp.a 2> temp.a";
                 std::string file1 = std::string("./") + argv[1] + arguments;
