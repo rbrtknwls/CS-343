5703f70f2856e9c624fd622d2cc76dd199652126
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-03 20:09:17 -0400

    more

--- tester.cpp
+++ tester.cpp
@@ -16,10 +16,14 @@ int main ( int argc, char * argv[] ) {
                 if ( z == 0 ) { arguments += "d "; }
                 else { arguments += std::to_string(z) + " "; }
 
-                arguments += " > temp.a 2> temp.a";
-                std::string file1 = std::string("./") + argv[1] + arguments;
+                std::string arg1 += " > temp.a 2> temp.a";
+                std::string arg2 += " > temp.b 2> temp.b";
+
+                std::string file1 = std::string("./") + argv[1] + arg1;
+                std::string file2 = std::string("./") + argv[2] + arg2;
 
                 system(file1.c_str());
+                system(file2.c_str());
             }
         }
     }
