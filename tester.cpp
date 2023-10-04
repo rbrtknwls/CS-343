a5b16a36476e6eb8c00fcac0d3dd303f95ad448e
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-03 20:00:50 -0400

    more

--- tester.cpp
+++ tester.cpp
@@ -2,7 +2,16 @@
 
 
 int main ( int argc, char * argv[] ) {
-    std::string file1 = std::string("./") + argv[1];
 
-    system(file1.c_str());
+    for ( int i = 0; i < 2; i++ ) {
+        for (int x = 0; x < 2; x++ ) {
+            for (int z = 0; z < 2; z++ ) {
+                std::string arguments = std::string(i) + " " + std::string(x) + " " + std::string(z) + " > temp.a";
+                std::string file1 = std::string("./") + argv[1] + arguments;
+
+                system(file1.c_str());
+            }
+        }
+    }
+
 }
