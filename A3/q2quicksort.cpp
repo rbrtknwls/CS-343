20e9f29186f44a21a9fbf9c6d5e74f3ca52a4c92
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-22 06:11:27 -0400

    starter

---
+++ A3/q2quicksort.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "q2quicksort.h"
+
+using namespace std;
+
+int main() {
+
+    cout << "hai" << endl;
+}
---
+++ A3/q2quicksort.h
@@ -0,0 +1,10 @@
+#ifndef CS_343_Q2QUICKSORT_H
+#define CS_343_Q2QUICKSORT_H
+
+
+class q2quicksort {
+
+};
+
+
+#endif //CS_343_Q2QUICKSORT_H
