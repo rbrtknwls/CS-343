b69e3700184d446717ce32ffcc6c5cf4738568c9
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-22 16:58:44 -0400

    more

--- A3/q2quicksort.h
+++ A3/q2quicksort.h
@@ -32,9 +32,13 @@ template<typename T> unsigned int partition ( T values[], unsigned int low, unsi
 
 template<typename T> void quicksort( T values[], unsigned int low, unsigned int high, unsigned int depth ) {
 
+  if (low >= high) { break; }
+
     int idx = partition(values, low, high);
 
-    
+    quicksort(values, low, idx-1, depth);
+    quicksort(values, idx+1, high, depth);
+
 
 }
 
