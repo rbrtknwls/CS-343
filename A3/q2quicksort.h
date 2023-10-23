2288033df1a740d5fc013a433260e54b1f04466a
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-22 21:49:47 -0400

    A

--- A3/q2quicksort.h
+++ A3/q2quicksort.h
@@ -16,12 +16,12 @@ template<typename T> void quicksort( T values[], unsigned int low, unsigned int 
     unsigned int idx = low;
 
     swap(values[pivotIdx], values[high]);
-    for ( unsigned int j = low; j < high; j++ ) {
+    for ( unsigned int j = low; j <= high; j++ ) {
         if ( values[j] < values[high] ) {
             swap(values[idx++], values[j]);
         }
     }
-    swap(values[idx], values[high]);
+    swap(values[idx++], values[high]);
 
 
     if ( depth == 0 ) {
