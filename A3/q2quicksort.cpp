a657182b50097eb464f2152ea15b0023f0933438
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-22 20:19:18 -0400

    second

--- A3/q2quicksort.cpp
+++ A3/q2quicksort.cpp
@@ -79,7 +79,7 @@ int main( int argc, char * argv[] ) {
         uProcessor p[ (1 << depth) - 1 ]; // 2^depth-1 kernel threads
 
         uTime start = uClock::currTime();
-        quicksort( arrayOfValues, 0, size-1, depth );
+        sequentialQuicksort( arrayOfValues, 0, size-1 );
         cout << "Sort time " << uClock::currTime() - start << " sec." << endl;
 
         delete[] arrayOfValues;
