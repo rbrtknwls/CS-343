065775acdca1d4b28b050242b18662a71d97ab66
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-23 02:32:15 -0400

    even more

--- A3/q3buffer.h
+++ A3/q3buffer.h
@@ -1,28 +1,23 @@
-//
-// Created by robde on 2023/10/23.
-//
-
 #ifndef CS_343_Q3BUFFER_H
 #define CS_343_Q3BUFFER_H
 
+#include "BargingCheck.h"
+#include iostream;
 
 #ifdef BUSY							// busy waiting implementation
 // implementation
 #endif // BUSY
 
 #ifdef NOBUSY
-#include "BargingCheck.h"
 template<typename T> class BoundedBuffer {
-
+    int blocks = 0;
   public:
-    _Event Poison {} {
-
-    }
+    _Event Poison {};
     BoundedBuffer( const unsigned int size = 10 ) {
 
     }
     unsigned long int blocks() {
-
+        return blocks;
     }
     void poison() {
 
