eb2713e23d6f474ff6201eb6f510ce017342d525
rbrtknwls <r2knowle@uwaterloo.ca>
2023-10-22 20:56:04 -0400

    E

--- A3/q2quicksort.h
+++ A3/q2quicksort.h
@@ -4,100 +4,92 @@
 #include <iostream>
 #include <uCobegin.h>
 #include <uActor.h>
-#include <bits/stdc++.h>
 
 using namespace std;
 
-template<typename T>
-void quicksort( T values[], unsigned int low, unsigned int high, unsigned int depth ) {
-    if (low < high) {
-
-        unsigned int pivotIdx = low + (high - low) / 2;
-        unsigned int localSwap = low;
-        /*
-        swap(values[pivotIdx], values[high]);
-        for ( unsigned int j = low; j < high; j++ ) {
-            if ( values[j] < values[high] ) {
-                swap(values[localSwap++], values[j]);
-            }
-        }
-        swap(values[localSwap], values[high]);
-        */
-        if ( localSwap != 0 ) { quicksort(values, low, localSwap - 1, depth); }
-        quicksort(values, localSwap + 1, high, depth);
-    }
+template<typename T> void swap ( T values[], unsigned int idx1, unsigned int idx2 ) {
+    T temp = values[idx1];
+    values[idx1] = values[idx2];
+    values[idx2] = temp;
 }
 
-/*
-template<typename T> void sequentialQuicksort( T &values, int low, int high ) {
-    if (high == (unsigned int)-1) return;
-  if (low < high) {
+template<typename T> unsigned int partition ( T values[], unsigned int low, unsigned int high ) {
+    unsigned int pivotIdx = low + (high - low) / 2;
+    unsigned int localSwap = low;
 
-      int pivotIdx = low + (high - low) / 2;
-      unsigned int localSwap = low;
+    swap(values, pivotIdx, high);
+    for ( unsigned int j = low; j < high; j++ ) {
+        if ( values[j] < values[high] ) {
+            swap(values, localSwap++, j);
+        }
+    }
+    swap(values, localSwap, high);
 
-      swap(values[pivotIdx], values[high]);
-      for (unsigned int j = low; j < high; j++) {
-          if (values[j] < values[high]) {
-              swap(values[localSwap++], values[j]);
-          }
-      }
-      swap(values[localSwap], values[high]);
+    return localSwap;
+}
 
-      sequentialQuicksort(values, low, localSwap - 1);
-      sequentialQuicksort(values, localSwap + 1, high);
-  }
+template<typename T> void sequentialQuicksort( T values[], unsigned int low, unsigned int high ) {
+    if (low >= high) { return; }
+
+    unsigned int idx = partition(values, low, high);
+
+    if ( idx > 0 ) {
+        sequentialQuicksort(values, low, idx - 1);
+    }
+    sequentialQuicksort(values, idx + 1, high);
+
 }
 
 #if defined( TASK )
 template<typename T> _Task SortWithTask {
     T *values;
-    int low;
-    int high;
-    int depth;
-
+    unsigned int low;
+    unsigned int high;
+    unsigned int depth;
 
     void main() {
 
       if ( low >= high ) { return; }
 
         if ( depth == 0 ) { sequentialQuicksort(values, low, high); } else {
-            int idx = partition(values, low, high);
-            SortWithTask left(values, low, idx - 1, depth-1);
+            unsigned int idx = partition(values, low, high);
+            if ( idx - 1 > 0 ) {
+                SortWithTask left(values, low, idx - 1, depth-1);
+            }
             sequentialQuicksort(values, idx + 1, high);
         }
     }
 
   public:
-    SortWithTask( T *values, int low, int high, int depth ) : values(values), low(low), high(high), depth(depth) {}
+    SortWithTask( T *values, unsigned int low, unsigned int high, unsigned int depth ) : values(values), low(low), high(high), depth(depth) {}
 };
-*/
-#if defined( ACTOR )
+#elif defined( ACTOR )
 
 struct SortMsg : public uActor::Message {
     STYPE *values;
-    int low;
-    int high;
-    int depth;
+    unsigned int low;
+    unsigned int high;
+    unsigned int depth;
 
-    SortMsg( STYPE *values, int low, int high, int depth ) :
+    SortMsg( STYPE *values, unsigned int low, unsigned int high, unsigned int depth ) :
         Message(uActor::Delete), values(values), low(low), high(high), depth(depth) {}
 };
 
 _Actor SortWithActor {
     Allocation receive( uActor::Message & msg ) {
         Case( SortMsg, msg ) {
-            int low = msg_d->low;
-            int high = msg_d->high;
-            int depth = msg_d->depth;
+            unsigned int low = msg_d->low;
+            unsigned int high = msg_d->high;
+            unsigned int depth = msg_d->depth;
 
 
 
             if ( depth == 0 || low >= high ) { sequentialQuicksort( msg_d->values, low, high ); } else {
 
-                int idx = partition(msg_d->values, low, high);
-
-                *new SortWithActor() | *new SortMsg( msg_d->values, low, idx - 1, depth-1 ) | uActor::stopMsg;
+                unsigned int idx = partition(msg_d->values, low, high);
+                if ( idx - 1 > 0 ) {
+                    *new SortWithActor() | *new SortMsg( msg_d->values, low, idx - 1, depth-1 ) | uActor::stopMsg;
+                }
                 *new SortWithActor() | *new SortMsg( msg_d->values, idx + 1, high, depth-1 ) | uActor::stopMsg;
 
 
@@ -109,23 +101,21 @@ _Actor SortWithActor {
 
 };
 
+#endif
 
 
-/*
-template<typename T> void quicksort( T *values, int low, int high, int depth ) {
-  if (low >= high) { return; }
-
-
-#if defined( CBEGIN )
+template<typename T> void quicksort( T values[], unsigned int low, unsigned int high, unsigned int depth ) {
+    if (low >= high) { return; }
 
     if ( depth == 0 ) { sequentialQuicksort(values, low, high); } else {
+
+#if defined( CBEGIN )
 
         int idx = partition(values, low, high);
         COBEGIN
             BEGIN quicksort( values, low, idx - 1, depth-1 ); END
             BEGIN quicksort( values, idx + 1, high, depth-1 ); END
         COEND
-    }
 
 #elif defined( TASK )
 
@@ -136,9 +126,10 @@ template<typename T> void quicksort( T *values, int low, int high, int depth ) {
         uActor::start();
         *new SortWithActor() | *new SortMsg( values, low, high, depth ) | uActor::stopMsg;
         uActor::stop();
-*/
-#endif
 
+#endif
+    }
+}
 
 
 
