218bc258b0f67ca524dec8388a31a297bda5c564
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-05 12:55:41 -0500

    has

--- A4/q3voter.cpp
+++ A4/q3voter.cpp
@@ -6,6 +6,6 @@
 Voter::Voter( unsigned int id, unsigned int nvotes, TallyVotes & voteTallier, Printer & printer ) :
     id(id), nvotes(nvotes), voteTallier(&voteTallier), printer(&printer) {}
 
-Voter::main() {
+void Voter::main() {
     p
 }
