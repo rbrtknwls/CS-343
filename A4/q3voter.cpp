ab31719b20cc2521f43098e8b941455b892414a4
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-05 12:55:18 -0500

    E

--- A4/q3voter.cpp
+++ A4/q3voter.cpp
@@ -1,4 +1,11 @@
+#include <iostream>
+
 #include "q3voter.h"
+
 
 Voter::Voter( unsigned int id, unsigned int nvotes, TallyVotes & voteTallier, Printer & printer ) :
     id(id), nvotes(nvotes), voteTallier(&voteTallier), printer(&printer) {}
+
+Voter::main() {
+    p
+}
