9153094ec5abe99bd21310cdde3be77be6897446
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-05 12:09:26 -0500

    OK

--- A4/q3.cpp
+++ A4/q3.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+#include "q3tallyVotes.h"
+#include "q3voter.h"
+#include "q3printer.h"
+/*
+ * Main runner for the tour group voting problem, this file will handle the input from the user and will set the
+ * states for the printer, voters and tally system.
+*/
+
 int main() {
     std::cout << "HI" << std::endl;
 }
--- A4/q3printer.cpp
+++ A4/q3printer.cpp
@@ -1,5 +1,1 @@
-//
-// Created by robde on 2023/11/05.
-//
-
 #include "q3printer.h"
