1dfd829eae8bf5aaea927e82f0cae443abf94502
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-05 11:05:55 -0500

    E

--- A4/q3tallyVotes.h
+++ A4/q3tallyVotes.h
@@ -1,6 +1,3 @@
-#ifndef CS_343_Q3TALLYVOTES_H
-#define CS_343_Q3TALLYVOTES_H
-
 #if defined( MC )					// mutex/condition solution
 #include "BargingCheckVote.h"
 // includes for this kind of vote-tallier
@@ -16,22 +13,20 @@ class TallyVotes {
 _Cormonitor TallyVotes : public uBarrier {
 	// private declarations for this kind of vote-tallier
 #else
-    #error unsupported voter type
+	#error unsupported voter type
 #endif
-// common declarations
+	// common declarations
   public:							// common interface
-    _Event Failed {};
-    struct Ballot { unsigned int picture, statue, giftshop; };
-    enum TourKind : char { Picture = 'p', Statue = 's', GiftShop = 'g' };
-    struct Tour { TourKind tourkind; unsigned int groupno; };
+	_Event Failed {};
+	struct Ballot { unsigned int picture, statue, giftshop; };
+	enum TourKind : char { Picture = 'p', Statue = 's', GiftShop = 'g' };
+	struct Tour { TourKind tourkind; unsigned int groupno; };
 
-    TallyVotes( unsigned int voters, unsigned int group, Printer & printer );
-    Tour vote( unsigned int id, Ballot ballot );
-    void done(
-    #if defined( MC ) || defined( BAR )
-        unsigned int id
-    #endif
-    );
+	TallyVotes( unsigned int voters, unsigned int group, Printer & printer );
+	Tour vote( unsigned int id, Ballot ballot );
+	void done(
+		#if defined( MC ) || defined( BAR )
+		unsigned int id
+		#endif
+	);
 };
-
-#endif
