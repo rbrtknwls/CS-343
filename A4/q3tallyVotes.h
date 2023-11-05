2082e45ca03edb26e5b373d428c052f493a336ca
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-05 10:57:56 -0500

    EE

--- A4/q3tallyVotes.h
+++ A4/q3tallyVotes.h
@@ -3,7 +3,6 @@
 
 #if defined( MC )					// mutex/condition solution
 #include "BargingCheckVote.h"
-// includes for this kind of vote-tallier
 class TallyVotes {
 	// private declarations for this kind of vote-tallier
 #elif defined( SEM )				// semaphore solution
