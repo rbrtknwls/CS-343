a6c1e9475d35efbf5e9b0454b320c1fdb7eb89dc
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-05 11:15:18 -0500

    printer!

--- A4/q3voter.h
+++ A4/q3voter.h
@@ -2,7 +2,7 @@
 #define CS_343_Q3VOTER_H
 
 #include "q3tallyVotes.h"
-
+#include "q3printer.h"
 _Task Voter {
     TallyVotes::Ballot cast() __attribute__(( warn_unused_result )) {  // cast 3-way vote
         // O(1) random selection of 3 items without replacement using divide and conquer.
