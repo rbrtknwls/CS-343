8914dcb3c33c3e1bea83926b87035142a577cdd3
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-05 12:53:49 -0500

    maybe

--- A4/q3voter.cpp
+++ A4/q3voter.cpp
@@ -1,4 +1,4 @@
 #include "q3voter.h"
 
 Voter::Voter( unsigned int id, unsigned int nvotes, TallyVotes & voteTallier, Printer & printer ) :
-    id(id), nvotes(nvotes), voteTallier(voteTallier), printer(printer) {}
+    id(id), nvotes(nvotes), voteTallier(&voteTallier), printer(&printer) {}
