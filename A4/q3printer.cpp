437b5134d7e921ddd0cf10ca9f3157aaf9be8d80
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-06 16:02:02 -0500

    double

--- A4/q3printer.cpp
+++ A4/q3printer.cpp
@@ -2,5 +2,9 @@
 
 Printer::Printer( unsigned int voters ) : voters(voters) {
     hasBeenWrittenTo = new bool[voters];
+
+    for ( int i = 0; i < voters; i++ ) {
+        hasBeenWrittenTo[i] = false;
+    }
 }
 
--- A4/q3printer.h
+++ A4/q3printer.h
@@ -4,17 +4,16 @@
 #include "q3tallyVotes.h"
 #include "q3voter.h"
 
-/*
-struct PrinterState {
-    Voter::States mainState,
-    unsigned int val1,
-    unsigned int val2,
-    unsigned int val3
-};*/
-
 _Monitor Printer {
+    struct PrinterState {
+        Voter::States mainState,
+        unsigned int val1,
+        unsigned int val2,
+        unsigned int val3
+    };
+
     unsigned int voters;
-    //PrinterState printBuffer*;
+    PrinterState *printBuffer;
     bool *hasBeenWrittenTo;
 
   public:
