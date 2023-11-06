92344a2499d0f31e2fabb4ba72a0156450aaba8f
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-06 17:07:01 -0500

    E

--- A4/q3printer.cpp
+++ A4/q3printer.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 void Printer::flushBuffer () {
     for ( int i = 0; i < voters; i++ ) {
-        if (hasBeenWrittenTo[i]) {
+        if ( hasBeenWrittenTo[i] ) {
+            Voter::State currentState = printBuffer[i].mainState;
+            std::cout << currentState;
+            
 
         } else {
             std::cout << "\t";
