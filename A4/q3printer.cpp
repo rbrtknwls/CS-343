cb800a9fab552346d307d14273c201cfd47bed74
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-06 16:31:26 -0500

    upload example

--- A4/q3printer.cpp
+++ A4/q3printer.cpp
@@ -1,4 +1,9 @@
 #include "q3printer.h"
+#include <iostream>
+
+void flushBuffer ( int voters, bool* hasBeenWrittenTo, PrinterState *buffer ) {
+
+}
 
 Printer::Printer( unsigned int voters ) : voters(voters) {
     hasBeenWrittenTo = new bool[voters];
@@ -6,6 +11,7 @@ Printer::Printer( unsigned int voters ) : voters(voters) {
 
 
     for ( unsigned int i = 0; i < voters; i++ ) {
+        std::cout <<
         hasBeenWrittenTo[i] = false;
     }
 }
