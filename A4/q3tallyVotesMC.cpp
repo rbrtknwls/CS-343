4dce2504a14cc4dba47129b0878b6bbb8682dc5f
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-08 22:17:48 -0500

    try again

--- A4/q3tallyVotesMC.cpp
+++ A4/q3tallyVotesMC.cpp
@@ -10,7 +10,7 @@ TallyVotes::Tour TallyVotes::vote( unsigned id, Ballot ballot ) {
         if ( votingGroupInProgress ) {
             numberOfBarging++;
 
-            //if ( numberOfBarging + currentNumberOfGroupMembers >= voters ) { bargingLock.signal(); }
+            if ( numberOfBarging + currentNumberOfGroupMembers == voters ) { bargingLock.signal(); }
             printer->print( id, Voter::Barging, currentGroupNumber, currentNumberOfGroupMembers );
 
             bargingLock.wait( tallyVotesLock );
