f136b4aa43fde61c707ca62d293cd706c1b0b860
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-21 18:15:01 -0500

    external

--- A5/q2tallyVotesEXT.cpp
+++ A5/q2tallyVotesEXT.cpp
@@ -2,8 +2,12 @@
 #include "q2voter.h"
 #include "q2printer.h"
 
+/*
+ * Semaphore implementation of tally voters, this contains both the vote method and the done method. All the other
+ *  methods that are needed for tally votes are implemented in the generic tallyVotes.cpp
+ */
 
-TallyVotes::Tour TallyVotes::vote( unsigned int id, Ballot ballot ) {
+TallyVotes::Tour TallyVotes::vote( unsigned id, Ballot ballot ) {
 
     printer->print( id, Voter::Vote, ballot );
 
@@ -11,11 +15,36 @@ TallyVotes::Tour TallyVotes::vote( unsigned int id, Ballot ballot ) {
     votes[1] += ballot.statue;
     votes[2] += ballot.giftshop;
 
+    currentNumberOfGroupMembers++;
+
+    if (currentNumberOfGroupMembers == maxGroupSize) { // Only apply for the last member
+        currentTour.tourkind = determineWinner();
+        currentTour.groupno = ++currentGroupNumber;
+
+        // Reset the number of votes
+        votes[0] = 0;
+        votes[1] = 0;
+        votes[2] = 0;
+
+        printer->print( id, Voter::Complete, currentTour );
+
+    } else {
+        printer->print( id, Voter::Block, currentNumberOfGroupMembers );
+
+        _Accept ( TallyVotes::vote )
+        if ( voters < maxGroupSize ) { _Throw Failed(); }
+
+        printer->print( id, Voter::Unblock, currentNumberOfGroupMembers - 1);
+    }
+    currentNumberOfGroupMembers--;
+
+
+    if ( voters < maxGroupSize ) { _Throw Failed(); }   // Quorum Failure
     return currentTour;
 
 }
 
-void TallyVotes::done( ) {
+void TallyVotes::done() {
     voters--;
 
     if ( voters < maxGroupSize ) {
