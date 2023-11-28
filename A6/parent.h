6dec063f6b34ea6ed5114c54334c4f2a0222c936
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-28 15:39:34 -0500

    E

--- A6/parent.h
+++ A6/parent.h
@@ -1,10 +1,12 @@
 #ifndef CS_343_PARENT_H
 #define CS_343_PARENT_H
 
+_Monitor Printer;
+
 _Task Parent {
-        void main();
-        public:
-        Parent( Printer & prt, Bank & bank, unsigned int numStudents, unsigned int parentalDelay );
+    void main();
+  public:
+    Parent( Printer & prt, Bank & bank, unsigned int numStudents, unsigned int parentalDelay );
 };
 
 
