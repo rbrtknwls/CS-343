659c2066b97bace250b763aeed55667920d65541
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-28 16:54:42 -0500

    update the priner

--- A6/printer.cpp
+++ A6/printer.cpp
@@ -1,1 +1,51 @@
 #include "printer.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// ================== Public Methods ==================== //
+
+Printer::Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers ) :
+        numStudents(numStudents), numVendingMachines(numVendingMachines), numCouriers(numCouriers) {
+
+    hasBeenWrittenTo = new bool[numStudents + numVendingMachines + numCouriers + NUMBEROFOTHERACTORS];
+    printBuffer = new PrinterState[numStudents + numVendingMachines + numCouriers + NUMBEROFOTHERACTORS];
+
+
+    for ( unsigned int id = 0; id < NUMBEROFOTHERACTORS; id++ ) {
+        hasBeenWrittenTo[i] = false;
+
+        string toPrint;
+        switch ( id ) {
+            case PARENTID:
+                toPrint = "Parent";
+                break;
+            case GROPOFFID:
+                toPrint = "Gropoff";
+                break;
+            case WATOFFID:
+                toPrint = "WATOff";
+                break;
+            case NAMESID:
+                toPrint = "Truck";
+                break;
+            case PLANTID:
+                toPrint = "Plant";
+                break;
+        }
+        nonSpaceValues += toPrint.length();
+
+        std << toPrint;
+        std << string(8 - nonSpaceValues, ' ');
+    }
+    cout << endl;
+
+    for ( unsigned int i = 0; i < numStudents + numVendingMachines + numCouriers + NUMBEROFOTHERACTORS; i++ ) {
+        cout << "*******";
+        if ( i != numStudents + numVendingMachines + numCouriers + NUMBEROFOTHERACTORS-1 ) { cout << " "; }
+    }
+
+    cout << endl;
+#endif
+}
--- A6/printer.h
+++ A6/printer.h
@@ -3,8 +3,28 @@
 
 
 _Monitor Printer {
+    struct PrinterState {
+        char mainState;
+        unsigned int value1, value2, lid;
+    };
 
+
+    const unsigned int NUMBEROFOTHERACTORS = 6;
+    const unsigned int PARENTID = 0;
+    const unsigned int GROPOFFID = 1;
+    const unsigned int WATOFFID = 2;
+    const unsigned int NAMESID = 3;
+    const unsigned int TRUCKID = 4;
+    const unsigned int PLANTID = 5;
+
+    unsigned int numStudents;
+    unsigned int numVendingMachines;
+    unsigned int numCouriers;
+
+    PrinterState *printBuffer;
+    bool *hasBeenWrittenTo;
   public:
+
     enum Kind { Parent, Groupoff, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
     Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
     void print( Kind kind, char state );
