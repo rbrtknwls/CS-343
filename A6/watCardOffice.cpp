b3580dc9f26078e078d73645a892c51adf812c31
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-30 12:32:42 -0500

    run forever?

--- A6/watCardOffice.cpp
+++ A6/watCardOffice.cpp
@@ -30,7 +30,7 @@ void WATCardOffice::main() {
             unsigned int amount = workToDo.back()->studentID;
             printer->print( Printer::WATCardOffice, 'T', id, amount );
         } or _Accept ( WATCardOffice::requestWork ) {}
-        _Else { break; }
+        _Else { }
     }
 
     while ( !workToDo.empty() ) { _Accept ( WATCardOffice::requestWork ) }
