95057aa4540942312d3aaf04ed9ab91c4dc46420
rbrtknwls <r2knowle@uwaterloo.ca>
2023-11-30 12:06:16 -0500

    try again"

--- A6/watCardOffice.cpp
+++ A6/watCardOffice.cpp
@@ -58,7 +58,7 @@ WATCardOffice::WATCardOffice( Printer & prt, Bank & bank, unsigned int numCourie
 }
 
 
-WATCardOffice::Job::Job( unsigned int studentID, unsigned int amount, WATCard *card, Printer printer  ) :
+WATCardOffice::Job::Job( unsigned int studentID, unsigned int amount, WATCard *card, Printer *printer  ) :
     studentID( studentID ), amount( amount ), card ( card ), printer( printer ) {}
 
 WATCardOffice::Job::~Job() { printer->print( Printer::WATCardOffice, 'W' ); }
