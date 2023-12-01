82fb6b572c071f7fd8cde6e374500b54d1e02f83
rbrtknwls <r2knowle@uwaterloo.ca>
2023-12-01 17:28:50 -0500

    try again

--- A6/watCardOffice.cpp
+++ A6/watCardOffice.cpp
@@ -10,14 +10,14 @@ void WATCardOffice::Courier::main() {
 
         WATCardOffice::Job * job = watCardOffice->requestWork();
       if ( job == nullptr ) { break; }
-        printer->print( Printer::Courier, localID, 't', job->studentID, job->amount );
+        //printer->print( Printer::Courier, localID, 't', job->studentID, job->amount );
 
         watCardOffice->bank->withdraw( job->studentID, job->amount );
 
         if ( prng(6) == 0 ) {
-            printer->print( Printer::Courier, localID, 'L', job->studentID );
+            //printer->print( Printer::Courier, localID, 'L', job->studentID );
         } else {
-            printer->print( Printer::Courier, localID, 'T', job->studentID, job->amount );
+            //printer->print( Printer::Courier, localID, 'T', job->studentID, job->amount );
         }
 
     }
