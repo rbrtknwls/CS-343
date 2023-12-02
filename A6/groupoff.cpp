ae4ed46ee411761acff2cdc53d884e3782e87053
rbrtknwls <r2knowle@uwaterloo.ca>
2023-12-01 19:17:48 -0500

    E

--- A6/groupoff.cpp
+++ A6/groupoff.cpp
@@ -23,7 +23,7 @@ WATCard::FWATCard Groupoff::giftCard() {
 // ================== Constructor / Destructor ==================== //
 
 Groupoff::Groupoff( Printer & prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupoffDelay ) :
-    printer( &prt ), numStudents( (int) numStudents ), sodaCost( sodaCost ), groupoffDelay(groupoffDelay) {
+    printer( &prt ), numStudents( (int) numStudents ), sodaCost( sodaCost ), groupoffDelay( groupoffDelay ) {
 
     currentStudent = 0;
     giftCards = new WATCard::FWATCard[numStudents];
@@ -32,6 +32,13 @@ Groupoff::Groupoff( Printer & prt, unsigned int numStudents, unsigned int sodaCo
 
 Groupoff::~Groupoff() {
 
+    for ( int student = 0; student < numStudents; student++ ) {
+        if ( giftCards[student].available ) {
+            delete giftCards[student]();
+        }
+    }
+
+    delete[] giftCards;
     printer->print( Printer::Groupoff, 'F' );
 
 }
