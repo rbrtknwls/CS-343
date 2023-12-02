b6726bccb4e9e8e72e27048570b6e31a90ba0f2c
rbrtknwls <r2knowle@uwaterloo.ca>
2023-12-01 19:04:38 -0500

    E

--- A6/groupoff.cpp
+++ A6/groupoff.cpp
@@ -4,24 +4,34 @@
 
 void Groupoff::main() {
 
+    for ( currentStudent ; currentStudent < numStudents ; currentStudent++ ) {
+        _Accept( giftCard );
+    }
+
+    for ( currentStudent ; currentStudent >= 0 ; currentStudent-- ) {
+
+    }
+
 }
 
 // ================== Public Member(s) ==================== //
 
 WATCard::FWATCard Groupoff::giftCard() {
-
+    return giftCards[currentStudent];
 }
 
 // ================== Constructor / Destructor ==================== //
 
 Groupoff::Groupoff( Printer & prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupoffDelay ) :
-    printer( prt ), numStudents( numStudents ), sodaCost( sodaCost ), groupoffDelay(groupoffDelay) {
+    printer( &prt ), numStudents( numStudents ), sodaCost( sodaCost ), groupoffDelay(groupoffDelay) {
 
-    printer->print( Printer::Groupoff, "S");
+    currentStudent = 0;
+    giftCards = new WATCard::FWATCard[numStudents];
+    printer->print( Printer::Groupoff, 'S' );
 }
 
 Groupoff::~Groupoff() {
 
-    printer->print( Printer::Groupoff, "F");
+    printer->print( Printer::Groupoff, 'F' );
 
 }
--- A6/groupoff.h
+++ A6/groupoff.h
@@ -1,16 +1,23 @@
 #ifndef CS_343_GROUPOFF_H
 #define CS_343_GROUPOFF_H
 
+#include <uPRNG.h>
+
 #include "groupoff.h"
 #include "printer.h"
+
+extern PRNG mainPRNG;
 
 _Task Groupoff {
     Printer *printer;
     unsigned int numStudents;
     unsigned int sodaCost;
     unsigned int groupoffDelay;
+    unsigned int currentStudent;
 
     void main();
+
+    WATCard::FWATCard* giftCards;
 
   public:
     Groupoff( Printer & prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupoffDelay );
