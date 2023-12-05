69e93db5d77b05cf88847c27a35b170106d7e257
rbrtknwls <r2knowle@uwaterloo.ca>
2023-12-05 09:14:07 -0500

    E

--- A6/watCard.h
+++ A6/watCard.h
@@ -8,7 +8,7 @@ class WATCard {
     WATCard( const WATCard && ) = delete;
     WATCard & operator=( const WATCard & ) = delete;
     WATCard & operator=( const WATCard && ) = delete;
-    unsigned int balance = 0;
+    int balance = 0;
 
   public:
     typedef Future_ISM<WATCard *> FWATCard;
