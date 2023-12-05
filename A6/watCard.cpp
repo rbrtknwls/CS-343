b99c6d84b7507a5f08d40fac97313a113f30be7e
rbrtknwls <r2knowle@uwaterloo.ca>
2023-12-05 09:14:44 -0500

    E

--- A6/watCard.cpp
+++ A6/watCard.cpp
@@ -2,8 +2,8 @@
 
 // ================== Public Member(s) ==================== //
 
-void WATCard::deposit( unsigned int amount ) { balance -= amount; }
-void WATCard::withdraw( unsigned int amount ) { balance += amount; }
+void WATCard::deposit( unsigned int amount ) { balance += amount; }
+void WATCard::withdraw( unsigned int amount ) { balance -= amount; }
 unsigned int WATCard::getBalance() { return balance; }
 
 // ================== Constructor / Destructor ==================== //
