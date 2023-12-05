992704d88a8baf1fe3b036a37cb54b192e0ae7ff
rbrtknwls <r2knowle@uwaterloo.ca>
2023-12-05 12:27:38 -0500

    vending machine comments

--- A6/vendingmachine.cc
+++ A6/vendingmachine.cc
@@ -8,37 +8,41 @@ void VendingMachine::main() {
 
     nameServer->VMregister(this);
 
-    LOOP: for(;;) {
+    LOOP: for( ;; ) {
+
         try {
-            _Accept(~VendingMachine) {
+            _Accept( ~VendingMachine ) {                      // Accept destructor and break out of main
                 break;
-            } or _Accept(buy) {
-                VMCard->withdraw(sodaCost);
-                stock[currFlavour]--;
-                printer->print(Printer::Kind::Vending, id, 'B', currFlavour, stock[currFlavour]);
-                wait.signalBlock();
-            } or _Accept(inventory) {
-                printer->print(Printer::Kind::Vending, id, 'r');
-                _Accept(restocked) {
-                    printer->print(Printer::Kind::Vending, id, 'R');
+
+            } or _Accept( buy ) {                             // Accept buying a soda
+                VMCard->withdraw( sodaCost );                 // Withdraw money from the card
+                stock[currFlavour]--;                         // Reduce stock of that flavour
+                printer->print( Printer::Kind::Vending, id, 'B', currFlavour, stock[currFlavour] );
+                wait.signalBlock();                           // Allow task to finish
+
+            } or _Accept( inventory ) {                       // Accept request for inventory
+                printer->print( Printer::Kind::Vending, id, 'r' );
+                _Accept( restocked ) {                        // Wait for restock to happen
+                    printer->print( Printer::Kind::Vending, id, 'R' );
                 } // _Accept
             } // _Accept
-        } catch (uMutexFailure::RendezvousFailure &) {
+
+        } catch ( uMutexFailure::RendezvousFailure & ) {      // if any members throw an exception
             switch(state) {
-                case VendingMachine::State::free:
-                    printer->print(Printer::Kind::Vending, id, 'A');
-                    stock[currFlavour]--;
+                case VendingMachine::State::free:             // When giving free drinks
+                    printer->print( Printer::Kind::Vending, id, 'A' );
+                    stock[currFlavour]--;                     // Reduce stock
                     break;
-                case VendingMachine::State::funds:
+                case VendingMachine::State::funds:            // When waiting for funds
                     break;
-                case VendingMachine::State::stocks:
-                    _Accept(inventory) {
-                        printer->print(Printer::Kind::Vending, id, 'r');
-                        _Accept(restocked) {
+                case VendingMachine::State::stocks:           // When out of stock
+                    _Accept( inventory ) {                    // Accept request for inventory
+                        printer->print(Printer::Kind::Vending, id, 'r' );
+                        _Accept( restocked ) {                // Accept request for restock
                             printer->print(Printer::Kind::Vending, id, 'R');
                         } // _Accept
 
-                    } or _Accept(~VendingMachine) {
+                    } or _Accept( ~VendingMachine ) {         // Accept destructor
                         break LOOP;
                     } // _Accept
                     break;
@@ -51,29 +55,31 @@ void VendingMachine::main() {
 } // VendingMachine::main
 
 
+// ================== Public Member(s) ==================== //
+
 
 void VendingMachine::buy( BottlingPlant::Flavours flavour, WATCard * card ) {
-    if (stock[flavour] == 0) {
+    if (stock[flavour] == 0) {                                  // If we are out of a flavour
         state = VendingMachine::State::stocks;
-        _Throw VendingMachine::Stock{};
+        _Throw VendingMachine::Stock{};                         // Throw stock exception
     } // if
 
-    if (card->getBalance() < sodaCost) {
+    if (card->getBalance() < sodaCost) {                        // If we dont have enough money
         state = VendingMachine::State::funds;
-        _Throw VendingMachine::Funds{};
+        _Throw VendingMachine::Funds{};                         // Throw funds exception
     } // if
 
-    VMCard = card;
-    currFlavour = flavour;
-    if (prng() % 4 == 0) {
+    // At this point we have enough funds, and theres enough stock of flavour
+
+    VMCard = card;                                              // Store card for later
+    currFlavour = flavour;                                      // Store flavour choice
+    if (prng() % 4 == 0) {                                      // 1 in 4 chance for it to be free
         state = VendingMachine::State::free;
-        _Throw VendingMachine::Free{};
+        _Throw VendingMachine::Free{};                          // Give soda for free
     } else {
-        wait.wait();
+        wait.wait();                                            // Wait for machine and card to be updated
     } // if
 } // VendingMachine::buy
-
-
 
 unsigned int * VendingMachine::inventory() {
     return stock;
@@ -90,7 +96,9 @@ _Nomutex unsigned int VendingMachine::getId() const {
     return id;
 } // VendingMachine::getId
 
+
 // ================== Constructor / Destructor ==================== //
+
 
 VendingMachine::VendingMachine( Printer & prt, NameServer & nameServer, unsigned int id, unsigned int sodaCost ):
 printer(&prt), nameServer( &nameServer ), id(id), sodaCost(sodaCost) {
